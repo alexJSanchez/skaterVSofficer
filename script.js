3283ab8504b1db26e5e16ae05a6e6abce58a3830
alex sanchez <alexjsanchez1020@gmail.com>
2024-06-05 23:28:39 -0400

    define variables

---
+++ script.js
@@ -0,0 +1,6 @@
+// Define HTML elements
+const board = document.getElementById("game-board");
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
+const score = document.getElementById("score");
+const highScoreText = document.getElementById("highScore");
