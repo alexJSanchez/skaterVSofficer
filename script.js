3feb8db272f3861b891a56b8e0fefb1808f09413
alex sanchez <alexjsanchez1020@gmail.com>
2024-06-06 00:13:33 -0400

    create movement for the human

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const gridSize = 20;
 let human = [{ x: 10, y: 10 }];
 let zombie = [{ x: 5, y: 5 }];
 let exit = generateExit()
+
 // let food = generateFood();
 let highScore = 0;
 let gameInterval;
@@ -71,4 +72,52 @@ function generateExit() {
 	const y = Math.floor(Math.random() * gridSize) + 1;
 	return [{ x:x, y:y }];
 }
+
+// Move human
+function moveHuman(dx, dy) {
+    const head = { x: human[0].x + dx, y: human[0].y + dy };
+    if (head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
+        return; // prevent moving out of bounds
+    }
+    human = [head];
+    draw();
+}
+
+// Handle keyboard events
+function handleKey(event) {
+    switch (event.key) {
+        case "ArrowUp":
+        case "w":
+            moveHuman(0, -1);
+            break;
+        case "ArrowDown":
+        case "s":
+            moveHuman(0, 1);
+            break;
+        case "ArrowLeft":
+        case "a":
+            moveHuman(-1, 0);
+            break;
+        case "ArrowRight":
+        case "d":
+            moveHuman(1, 0);
+            break;
+        case "i": // up-right
+            moveHuman(1, -1);
+            break;
+        case "j": // down-left
+            moveHuman(-1, 1);
+            break;
+        case "k": // down-right
+            moveHuman(1, 1);
+            break;
+        case "l": // up-left
+            moveHuman(-1, -1);
+            break;
+    }
+}
+
+document.addEventListener("keydown", handleKey);
+
+
 draw()
