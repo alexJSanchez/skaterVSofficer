f1d36c038c20b709098f83fa960a10e595ff85bb
alex sanchez <alexjsanchez1020@gmail.com>
2024-06-05 23:29:56 -0400

    draw map

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const instructionText = document.getElementById("instruction-text");
 const logo = document.getElementById("logo");
 const score = document.getElementById("score");
 const highScoreText = document.getElementById("highScore");
+
+
+// Define game variables
+const gridSize = 20;
+let snake = [{ x: 10, y: 10 }];
+// let food = generateFood();
+let highScore = 0;
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+
+
+// Draw game map, snake, food
+function draw() {
+	board.innerHTML = "";
+
+}
